ae6dce98c6826f36cbdb14c4304064b61bce5781
ikhda <ikhdamuhammad@gmail.com>
2019-07-25 22:33:42 +0700

    add to json

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('views', './views'); // specify the folders views
 app.set('view engine', 'ejs'); // set view engine to ejs
@@ -9,10 +12,31 @@ app.set('view engine', 'ejs'); // set view engine to ejs
 app.get('/', (req, res) => {
   fs.readFile('./data/data.json', (err, data) => {
     if (err) console.log(err);
-  
+
     const parsedData = JSON.parse(data);
-    
-    res.render('user/index', { data: parsedData });
+
+    res.render('index', { data: parsedData });
+  });
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/add', (req, res) => {
+  const { name } = req.body;
+
+  fs.readFile('./data/data.json', (err, data) => {
+    if (err) console.log(err);
+
+    const parsedData = JSON.parse(data);
+    const newData = [...parsedData, { name: name }];
+
+    fs.writeFile('./data/data.json', JSON.stringify(newData), err => {
+      if (err) console.log(err);
+
+      res.redirect('/');
+    });
   });
 });
 
